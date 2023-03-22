99c3a0eb7fbbee6d2ab516f4e93e3581a66d3b7c
Adail Spinola Horst <the.spaww@gmail.com>
2023-03-22 07:23:28 +0000

    Update script for add log lines and old r1-log.md to reflect the real challenge status (currently in round 2, not round 1)

--- addDay.sh
+++ addDay.sh
@@ -10,7 +10,7 @@ PROGRESS=${1:-''}
 THOUGHTS=${2:-''}
 URL=${3:-''}
 DAY=${4:-$(date +%m/%d)}
-LOG_FILE=${5:-'r1-log.md'}
+LOG_FILE=${5:-'r2-log.md'}
 
 # set -x
 LAST=$(expr $(grep '###' "$LOG_FILE" | tail -n1 | cut -d "D" -f 2 | cut -d " " -f 1) + 1)
